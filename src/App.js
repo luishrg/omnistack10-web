71c1f19e64fb7d7b659b92ff145f281d57849c00
Luis Rodrigues <luishumberto2000@gmail.com>
2020-02-16 20:56:42 -0300

    refact: components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from "react";
+import api from "./services/api";
+
+import DevItem from "./components/DevItem/index";
+import DevForm from "./components/DevForm/index";
 
 import "./Global.css";
 import "./App.css";
@@ -6,143 +10,35 @@ import "./Sidebar.css";
 import "./Main.css";
 
 function App() {
-  const [github_username, setGithub_username] = useState("");
-  const [techs, setTechs] = useState("");
-
-  const [latitude, setLatitude] = useState("");
-  const [longitude, setLongitude] = useState("");
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      position => {
-        const { latitude, longitude } = position.coords;
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      err => {
-        console.log(err);
-      },
-      {
-        timeout: 30000
-      }
-    );
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data.devs);
+    }
+
+    loadDevs();
   }, []);
 
-  async function handleAddDev(e) {
-    e.preventDefault();
+  async function handleAddDev(data) {
+    const response = (await api.post("/devs", data)).data;
+
+    setDevs([...devs, response]);
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input
-              name="github_username"
-              id="github_username"
-              required
-              value={github_username}
-              onChange={e => setGithub_username(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={e => setTechs(e.target.value)}
-            />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                required
-                value={latitude}
-                onChange={e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                name="longitude"
-                id="longitude"
-                type="number"
-                required
-                value={longitude}
-                onChange={e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars1.githubusercontent.com/u/49236151?s=460&v=4"
-                alt="Luis Rodrigues"
-              />
-              <div className="user-info">
-                <strong>Luis Rodrigues</strong>
-                <span>Angular, MongoDb, Express, NodeJS</span>
-              </div>
-            </header>
-            <p>
-              Desenvolvedor Javascript Full Stack na empresa Predict.info.
-              Trabalhando atualmente com a Mean Stack
-            </p>
-            <a href="https://github.com/luishrg">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars1.githubusercontent.com/u/49236151?s=460&v=4"
-                alt="Luis Rodrigues"
-              />
-              <div className="user-info">
-                <strong>Luis Rodrigues</strong>
-                <span>Angular, MongoDb, Express, NodeJS</span>
-              </div>
-            </header>
-            <p>
-              Desenvolvedor Javascript Full Stack na empresa Predict.info.
-              Trabalhando atualmente com a Mean Stack
-            </p>
-            <a href="https://github.com/luishrg">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars1.githubusercontent.com/u/49236151?s=460&v=4"
-                alt="Luis Rodrigues"
-              />
-              <div className="user-info">
-                <strong>Luis Rodrigues</strong>
-                <span>Angular, MongoDb, Express, NodeJS</span>
-              </div>
-            </header>
-            <p>
-              Desenvolvedor Javascript Full Stack na empresa Predict.info.
-              Trabalhando atualmente com a Mean Stack
-            </p>
-            <a href="https://github.com/luishrg">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
