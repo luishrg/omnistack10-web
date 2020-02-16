785c52868103ee0bce5a8bdd1ad6ed05538b4ec6
Luis Rodrigues <luishumberto2000@gmail.com>
2020-02-16 15:59:22 -0300

    create: stylesheet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./Global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars1.githubusercontent.com/u/49236151?s=460&v=4"
+                alt="Luis Rodrigues"
+              />
+              <div className="user-info">
+                <strong>Luis Rodrigues</strong>
+                <span>Angular, MongoDb, Express, NodeJS</span>
+              </div>
+            </header>
+            <p>
+              Desenvolvedor Javascript Full Stack na empresa Predict.info.
+              Trabalhando atualmente com a Mean Stack
+            </p>
+            <a href="https://github.com/luishrg">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars1.githubusercontent.com/u/49236151?s=460&v=4"
+                alt="Luis Rodrigues"
+              />
+              <div className="user-info">
+                <strong>Luis Rodrigues</strong>
+                <span>Angular, MongoDb, Express, NodeJS</span>
+              </div>
+            </header>
+            <p>
+              Desenvolvedor Javascript Full Stack na empresa Predict.info.
+              Trabalhando atualmente com a Mean Stack
+            </p>
+            <a href="https://github.com/luishrg">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img
+                src="https://avatars1.githubusercontent.com/u/49236151?s=460&v=4"
+                alt="Luis Rodrigues"
+              />
+              <div className="user-info">
+                <strong>Luis Rodrigues</strong>
+                <span>Angular, MongoDb, Express, NodeJS</span>
+              </div>
+            </header>
+            <p>
+              Desenvolvedor Javascript Full Stack na empresa Predict.info.
+              Trabalhando atualmente com a Mean Stack
+            </p>
+            <a href="https://github.com/luishrg">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
